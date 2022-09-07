144219f262e8945501cbf165115af8bca2e6a7a7
Umut Yurdugül <umtyrdql@gmail.com>
2022-09-08 00:59:46 +0300

    Create FirstProject.rb

---
+++ FirstProject.rb
@@ -0,0 +1,8 @@
+puts "Enter a number : "
+number1 =gets.chomp.to_i()
+puts "Enter the other number : "
+number2 = gets.chomp.to_i()
+puts(number1 + number2)
+puts(number1 - number2)
+puts(number1 / number2)
+puts(number1 * number2)
